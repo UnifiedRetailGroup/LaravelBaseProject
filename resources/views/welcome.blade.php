dd56b04206c55dec0b79fc1b939d0457c9f7c184
GarethL <gareth.lucas@unifiedretailgroup.com>
2015-07-13 17:04:07 +0100

    initial commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5</div>
+                <div class="title">URG Laravel 5 Base App</div>
             </div>
         </div>
     </body>
